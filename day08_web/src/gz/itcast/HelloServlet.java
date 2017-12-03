eb6562221267423917419ada9d88dbbd3c867836
liuwenzhu <18339967951@163.com>
2017-12-03 11:51:39 +0800

    update
    Signed-off-by: liuwenzhu <18339967951@163.com>

---
+++ day08_web/src/gz/itcast/HelloServlet.java
@@ -0,0 +1,33 @@
+package gz.itcast;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		
+		//向浏览器输出内容
+		response.getWriter().write("这是第一个动态网站" + new Date());
+	}
+
+}
